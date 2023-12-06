36438c4d545f75c16ecada26115190052592851f
Gavin Jakubik <gavito8800@gmail.com>
2023-12-06 17:08:07 -0500

    Day 2 complete

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,51 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+
+const log = (label: string, value: any) =>
+  console.log(chalk.bgBlue(`${label}: `), chalk.blue(value));
+
+const RED_CUBES = 12;
+const GREEN_CUBES = 13;
+const BLUE_CUBES = 14;
+
+const MAX_COLORS = {
+  red: RED_CUBES,
+  green: GREEN_CUBES,
+  blue: BLUE_CUBES,
+};
 
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+  data.forEach((line) => {
+    log('line', line);
+    const [gameIDstr, games] = line.split(':');
+    const gameID = gameIDstr.split(' ')[1];
+    let valid = true;
+    log('gameID', gameID);
+    log('games', games);
+    games.split(';').every((game) => {
+      game.split(',').every((colorStr) => {
+        const [num, color] = _.trim(colorStr).split(' ');
+        if (num > MAX_COLORS[color]) {
+          valid = false;
+          return false;
+        }
+        return true;
+      });
+      log('game', `${game} ${valid}`);
+      if (!valid) {
+        return false;
+      }
+      return true;
+    });
+    if (valid) {
+      total += parseInt(gameID);
+    }
+  });
+  return total;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,48 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+
+const log = (label: string, value: any) =>
+  console.log(chalk.bgBlue(`${label}: `), chalk.blue(value));
+
+const RED_CUBES = 12;
+const GREEN_CUBES = 13;
+const BLUE_CUBES = 14;
+
+const MAX_COLORS = {
+  red: RED_CUBES,
+  green: GREEN_CUBES,
+  blue: BLUE_CUBES,
+};
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+  data.forEach((line) => {
+    log('line', line);
+    const [gameIDstr, games] = line.split(':');
+    const gameID = gameIDstr.split(' ')[1];
+    let valid = true;
+    log('gameID', gameID);
+    log('games', games);
+    const minCubes = {
+      red: 0,
+      blue: 0,
+      green: 0,
+    };
+    games.split(';').forEach((game) => {
+      game.split(',').forEach((colorStr) => {
+        const [num, color] = _.trim(colorStr).split(' ');
+        if (parseInt(num) > minCubes[color]) {
+          minCubes[color] = parseInt(num);
+        }
+      });
+      log('game', `${game} ${valid}`);
+    });
+    total += Object.values(minCubes).reduce((a, b) => a * b);
+  });
+  return total;
 }
 
 const answer = await day2b();
