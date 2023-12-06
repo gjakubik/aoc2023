9df206aa5df6133befaf5f3b1641fa58da6c2f34
Gavin Jakubik <gavito8800@gmail.com>
2023-12-06 00:55:19 -0500

    Day 1 complete

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -1,9 +1,47 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+// const list = [ '1721', '979', '366', '299', '675', '1456' ]
+// list[0] ==== 1721
+
+function is_numeric(str: string) {
+  return /^\d+$/.test(str);
+}
+
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  console.log(chalk.bgRed('Your Data:'), chalk.red(data));
+
+  let total = 0;
+  data.forEach((line) => {
+    let numString = '';
+    Array.from(line).every((char) => {
+      if (is_numeric(char)) {
+        numString += char;
+        return false;
+      }
+
+      return true;
+    });
+
+    let searchIndex = line.length - 1;
+
+    while (searchIndex >= 0) {
+      if (is_numeric(line[searchIndex])) {
+        numString += line[searchIndex];
+        break;
+      }
+
+      searchIndex = searchIndex - 1;
+    }
+
+    const parsedValue = parseInt(numString);
+
+    total += parsedValue;
+  });
+
+  return total;
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,9 +1,57 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const NUMBERS = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+function is_numeric(str: string) {
+  return /^\d+$/.test(str);
+}
+
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  console.log(chalk.bgRed('Your Data:'), chalk.red(data));
+
+  let total = 0;
+  data.forEach((line, index) => {
+    console.log(chalk.bgRed(`String ${index}:`), chalk.red(line));
+
+    const digits = [];
+    // loop through each character in the string
+    Array.from(line).forEach((char, index) => {
+      // if the character is numeric, add it to the digits array
+      if (is_numeric(char)) {
+        digits.push(char);
+      } else {
+        // if the character is not numeric, check if it's a number word
+        Object.keys(NUMBERS).forEach((key) => {
+          // if the character is a number word, add it to the digits array
+          if (line.substring(index, index + key.length) === key) {
+            digits.push(NUMBERS[key]);
+          }
+        });
+      }
+    });
+
+    // add the first and last digit as strings
+    const first = digits[0];
+    const last = digits[digits.length - 1];
+    const firstLast = `${first}${last}`;
+    total += parseInt(firstLast);
+  });
+
+  return total;
 }
 
 const answer = await day1b();
