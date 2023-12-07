dfe4ef9d8b5acdeab56c4ec76da919c1830ebd1a
Gavin Jakubik <gavito8800@gmail.com>
2023-12-07 02:54:39 -0500

    Day 4 complete

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,9 +1,43 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+import { log, logRed, logYellow } from '../utils.ts';
+
+const getNumbers = (numStr: string) =>
+  _.trim(numStr)
+    .split(' ')
+    .filter((s) => _.trim(s) !== '')
+    .map((s) => parseInt(s));
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+  data.forEach((line) => {
+    const [winningStr, mineStr] = line.split(':')[1].split('|');
+
+    const winning = getNumbers(winningStr);
+    const mine = getNumbers(mineStr);
+
+    log('winning', winning);
+    log('mine', mine);
+
+    let wins = 0;
+    mine.forEach((myNum) => {
+      if (winning.includes(myNum)) {
+        logRed('match', myNum);
+        wins++;
+      }
+    });
+    if (wins > 0) {
+      const score = 1 * 2 ** (wins - 1);
+      logYellow('wins', wins);
+      logYellow('score', score);
+      total += score;
+    }
+  });
+
+  return total;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,9 +1,56 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+import { log, logRed, logYellow } from '../utils.ts';
+
+const getNumbers = (numStr: string) =>
+  _.trim(numStr)
+    .split(' ')
+    .filter((s) => _.trim(s) !== '')
+    .map((s) => parseInt(s));
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const memoTable = {};
+
+  const totalScratchcards = (lineIndex: number) => {
+    if (lineIndex >= data.length) {
+      return 0;
+    }
+
+    if (memoTable[lineIndex]) {
+      return memoTable[lineIndex];
+    }
+
+    const [winningStr, mineStr] = data[lineIndex].split(':')[1].split('|');
+
+    const winning = getNumbers(winningStr);
+    const mine = getNumbers(mineStr);
+
+    let wins = 0;
+    mine.forEach((myNum) => {
+      if (winning.includes(myNum)) {
+        wins++;
+      }
+    });
+
+    // Find total cards won from this card
+    let totalCards = 1;
+    if (wins > 0) {
+      _.range(lineIndex + 1, lineIndex + wins + 1).forEach((newCardIndex) => {
+        totalCards += totalScratchcards(newCardIndex);
+      });
+    }
+    memoTable[lineIndex] = totalCards;
+    return totalCards;
+  };
+
+  let total = 0;
+  data.forEach((_, index) => {
+    total += totalScratchcards(index);
+  });
+  return total;
 }
 
 const answer = await day4b();
