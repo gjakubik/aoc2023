e6e3ef70c1c89573975953949f5e4a946fab2e64
Gavin Jakubik <gavito8800@gmail.com>
2023-12-07 15:35:00 -0500

    Day 6 complete

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -1,9 +1,33 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+import { log, getNumbers } from '../utils.ts';
 
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const records = getNumbers(data[1].split(':')[1]);
+  const races = getNumbers(data[0].split(':')[1]).map((time, i) => ({
+    time,
+    record: records[i],
+    possibilities: 0,
+  }));
+
+  log('races', races);
+
+  races.forEach(({ time, record }, raceIndex) => {
+    _.range(1, time).forEach((holdTime) => {
+      if ((time - holdTime) * holdTime > record) {
+        races[raceIndex].possibilities++;
+      }
+    });
+  });
+
+  const total = races
+    .map((r) => r.possibilities)
+    .reduce((total, curr) => total * curr);
+
+  return total;
 }
 
 const answer = await day6a();
--- puzzles/utils.ts
+++ puzzles/utils.ts
@@ -1,14 +1,25 @@
 import chalk from 'chalk';
 import _ from 'lodash';
 
-export const log = (label: string, value?: any) =>
-  console.log(chalk.bgBlue(`${label}: `), chalk.blue(value));
+const FLATTEN_OBJS = ['object', 'list'];
+
+export const log = (
+  label: string,
+  value?: any,
+  labelColor: chalk.ChalkFunction = chalk.bgBlue,
+  valueColor: chalk.ChalkFunction = chalk.blue
+) => {
+  if (FLATTEN_OBJS.includes(typeof value)) {
+    value = JSON.stringify(value);
+  }
+  console.log(labelColor(`${label}: `), valueColor(value));
+};
 
 export const logYellow = (label: string, value?: any) =>
-  console.log(chalk.bgYellow(`${label}: `), chalk.yellow(value));
+  log(label, value, chalk.bgYellow, chalk.yellow);
 
 export const logRed = (label: string, value?: any) =>
-  console.log(chalk.bgRed(`${label}: `), chalk.red(value));
+  log(label, value, chalk.bgRed, chalk.red);
 
 export const isNumeric = (str: string) => {
   return /^\d+$/.test(str);
