f7ad064a863ae9fa115309fbd350f39f73207641
Gavin Jakubik <gavito8800@gmail.com>
2023-12-09 02:27:25 -0500

    Day 9 complete

--- puzzles/day-9/day-9-a.ts
+++ puzzles/day-9/day-9-a.ts
@@ -1,9 +1,50 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+import { getNumbers, log, logRed, logYellow } from '../utils.ts';
 
 export async function day9a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let total = 0;
+  data.forEach((line) => {
+    const nums = [getNumbers(line)];
+
+    // log('nums', nums);
+
+    while (!nums[nums.length - 1].every((n) => n === 0)) {
+      let prevNum = null;
+      const newNums = [];
+      nums[nums.length - 1].forEach((n) => {
+        if (prevNum !== null) {
+          newNums.push(n - prevNum);
+        }
+        prevNum = n;
+      });
+      nums.push(newNums);
+    }
+
+    // logRed('nums', nums);
+
+    let numLine = nums.length - 1;
+    while (numLine >= 0) {
+      if (nums[numLine].every((n) => n === 0)) {
+        nums[numLine].push(0);
+        numLine--;
+        continue;
+      }
+
+      nums[numLine].push(
+        nums[numLine][nums[numLine].length - 1] +
+          nums[numLine + 1][nums[numLine + 1].length - 1]
+      );
+      numLine--;
+    }
+
+    // logYellow('nums', nums);
+    total += nums[0][nums[0].length - 1];
+  });
+
+  return total;
 }
 
 const answer = await day9a();
--- puzzles/day-9/day-9-b.ts
+++ puzzles/day-9/day-9-b.ts
@@ -1,9 +1,47 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import _ from 'lodash';
+import { getNumbers, log, logRed, logYellow } from '../utils.ts';
 
 export async function day9b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let total = 0;
+  data.forEach((line) => {
+    const nums = [getNumbers(line)];
+
+    // log('nums', nums);
+
+    while (!nums[nums.length - 1].every((n) => n === 0)) {
+      let prevNum = null;
+      const newNums = [];
+      nums[nums.length - 1].forEach((n) => {
+        if (prevNum !== null) {
+          newNums.push(n - prevNum);
+        }
+        prevNum = n;
+      });
+      nums.push(newNums);
+    }
+
+    // logRed('nums', nums);
+
+    let numLine = nums.length - 1;
+    while (numLine >= 0) {
+      if (nums[numLine].every((n) => n === 0)) {
+        nums[numLine].unshift(0);
+        numLine--;
+        continue;
+      }
+
+      nums[numLine].unshift(nums[numLine][0] - nums[numLine + 1][0]);
+      numLine--;
+    }
+
+    // logYellow('nums', nums);
+    total += nums[0][0];
+  });
+
+  return total;
 }
 
 const answer = await day9b();
